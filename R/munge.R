af93fcdcfdb788a3f931535854257b2e51849466
Dan Evans <danev1@gmail.com>
2020-06-18 12:31:32 -0700

    updated ss tables

--- R/munge.R
+++ R/munge.R
@@ -17,6 +17,14 @@ table_reader <- function(x){
                  "PvalCohort", "tStat", "tStat_males", "tStat_females", "tStat_manyCont")
   table1 <- table1 %>%
     select(-any_of(drop_cols)) 
+  if(str_detect(x, "femur")){
+    drop_traits <- c("Av.Obj.Ar", "Av.Obj.ECDa", "Av.MMI.x.", "Av.MMI.y.", 
+                     "Av.MMI.max.", "Av.MMI.min.", "Crd.X", "Crd.Y", "Crd.Z",
+                     "i.S", "i.S.TS.per.", "Obj.N", "Tb.Th.pl.", "Tb.Sp.pl.",
+                     "Tb.N.pl.", "Tb.Dm.rd.", "Tb.Sp.rd.", "Tb.N.rd.", "Tb.Pf")
+    table1 <- table1 %>%
+      filter(!(trait %in% drop_traits))
+  }
   if(str_detect(x, "controls")){
     table1 <- table1 %>%
       rename(N_total = Nunique, N_male_mice = Nunique_males, N_female_mice = Nunique_females)
@@ -73,6 +81,18 @@ dataset <- "femur"
 mypathIn <- paste0("data_raw/tables_power/", dataset)
 mypathOut <- paste0("data/tables_power/", dataset, "/tables_ss.rds")
 myfiles <- list.files(path = mypathIn, pattern = ".csv", full.names = TRUE)
+
+if(sum(str_detect(myfiles, "femur")) > 0){
+  drop_traits <- c("Av.Obj.Ar", "Av.Obj.ECDa", "Av.MMI.x.", "Av.MMI.y.", 
+                   "Av.MMI.max.", "Av.MMI.min.", "Crd.X", "Crd.Y", "Crd.Z",
+                   "i.S", "i.S.TS.per.", "Obj.N", "Tb.Th.pl.", "Tb.Sp.pl.",
+                   "Tb.N.pl.", "Tb.Dm.rd.", "Tb.Sp.rd.", "Tb.N.rd.", "Tb.Pf")
+  myfiles <- myfiles[str_detect(myfiles, pattern = "Av.*", negate = TRUE)]
+  myfiles <- myfiles[str_detect(myfiles, pattern = "Crd.*", negate = TRUE)]
+  myfiles <- myfiles[str_detect(myfiles, pattern = "i.S.*", negate = TRUE)]
+  myfiles <- myfiles[str_detect(myfiles, pattern = fixed("Obj.N"), negate = TRUE)]
+  myfiles <- myfiles[str_detect(myfiles, pattern = "Tb.*", negate = TRUE)]
+}
 
 table_reader <- function(x){
   table1 <- read_csv(x) 
